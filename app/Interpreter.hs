488bf689a9a6f3bb6d3b837f6826aea6681904d0
loreloc <lorenzoloconte@outlook.it>
2020-12-02 21:07:08 +0100

    a working interpreter

--- app/Interpreter.hs
+++ app/Interpreter.hs
@@ -7,18 +7,24 @@ import Exception
       Result(Ok, Error),  )
 import Grammar
     ( ArithmeticExpr(Add, Sub, Mul, Constant, Identifier),
+      BooleanExpr(Boolean, Not, Or, And, Equal, LessEqual),
       VariableDecl(IntegerDecl),
       Command(Skip, Assignment, Branch, Loop),
       Program(Program) )
+import Dictionary ( Dictionary, empty, insert, search )
 
 -- |The state (memory) of the interpreter
-type State = [(String, Int)]
+type State = Dictionary String Int
+
+-- |Create an empty state
+emptyState :: State
+emptyState = empty
 
 -- |Evaluate an arithmetic expression given a state
 evalArithmetic :: State -> ArithmeticExpr -> Result Int
 evalArithmetic _ (Constant v) = Ok v
 evalArithmetic s (Identifier d) =
-    case lookup d s of
+    case search s d of
         Just v -> Ok v
         Nothing -> Error (UndeclaredVariable d)
 evalArithmetic s (Add a1 a2) = liftA2 (+) v1 v2
@@ -28,6 +34,23 @@ evalArithmetic s (Sub a1 a2) = liftA2 (-) v1 v2
     where v1 = evalArithmetic s a1
           v2 = evalArithmetic s a2
 evalArithmetic s (Mul a1 a2) = liftA2 (*) v1 v2
+    where v1 = evalArithmetic s a1
+          v2 = evalArithmetic s a2
+
+-- |Evaluate a boolean expression given a state
+evalBoolean :: State -> BooleanExpr -> Result Bool
+evalBoolean _ (Boolean t) = Ok t
+evalBoolean s (Not b) = not <$> evalBoolean s b
+evalBoolean s (Or b1 b2) = liftA2 (||) t1 t2
+    where t1 = evalBoolean s b1
+          t2 = evalBoolean s b2
+evalBoolean s (And b1 b2) = liftA2 (&&) t1 t2
+    where t1 = evalBoolean s b1
+          t2 = evalBoolean s b2
+evalBoolean s (Equal a1 a2) = liftA2 (==) v1 v2
+    where v1 = evalArithmetic s a1
+          v2 = evalArithmetic s a2
+evalBoolean s (LessEqual a1 a2) = liftA2 (<=) v1 v2
     where v1 = evalArithmetic s a1
           v2 = evalArithmetic s a2
 
@@ -47,17 +70,30 @@ executeVariables s (v : vs) = s''
 executeCommands :: State -> [Command] -> State
 executeCommands s [] = s
 executeCommands s (Skip : cs) = executeCommands s cs
-executeCommands s ((Assignment d a) : cs) = []  -- TODO
-executeCommands s ((Branch b cs' cs'') : cs) = []  -- TODO
-executeCommands s ((Loop b cs') : cs) = [] -- TODO
+executeCommands s ((Assignment d a) : cs) =
+    case search s d of
+        Just _ -> case evalArithmetic s a of
+            Ok v -> executeCommands s' cs
+                where s' = insert s d v
+            Error e -> throw e
+        Nothing -> throw (UndeclaredVariable d)
+executeCommands s ((Branch b cs' cs'') : cs) =
+    case evalBoolean s b of
+        Ok True -> executeCommands s (cs' ++ cs)
+        Ok False -> executeCommands s (cs'' ++ cs)
+        Error e -> throw e
+executeCommands s ((Loop b cs') : cs) =
+    case evalBoolean s b of
+        Ok True -> executeCommands s' ((Loop b cs') : cs)
+            where s' = executeCommands s cs'
+        Ok False -> executeCommands s cs
+        Error e -> throw e
 
 -- |Augment a state by declaring a variable
 augmentState :: State -> VariableDecl -> State
 augmentState s (IntegerDecl d a) =
-    case p of
+    case search s d of
         Just _ -> throw (MultipleDeclaration d)
-        Nothing -> case q of
-            Ok v -> (d, v) : s
+        Nothing -> case evalArithmetic s a of
+            Ok v -> insert s d v
             Error e -> throw e
-    where p = lookup d s
-          q = evalArithmetic s a
--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,16 @@ module Main where
 
 import Grammar
     ( Program(Program),
-      Command(Assignment),
+      Command(Assignment, Loop),
       VariableDecl(IntegerDecl),
-      ArithmeticExpr(Constant) )
+      ArithmeticExpr(Constant, Add, Mul, Identifier),
+      BooleanExpr(LessEqual) )
+import Interpreter
+    ( emptyState, execute)
 
 main :: IO ()
 main = do
-    let p = Program [IntegerDecl "ciao" (Constant 1816)] [Assignment "ciao" (Constant 42)]
+    let p = Program [IntegerDecl "i" (Constant 1), IntegerDecl "n" (Constant 5), IntegerDecl "x" (Constant 1)] [Loop (LessEqual (Identifier "i") (Identifier "n")) [Assignment "x" (Mul (Identifier "x") (Identifier "i")), Assignment "i" (Add (Identifier "i") (Constant 1))]]
+    let s = execute emptyState p
     print p
+    print s
