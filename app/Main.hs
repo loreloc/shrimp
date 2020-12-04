6febf0d05ddae4de42ef69c6a1a133d86aaa15db
loreloc <lorenzoloconte@outlook.it>
2020-12-04 21:16:40 +0100

    simplified package.yaml

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,5 @@
 module Main where
 
-import Shrimp.Grammar
-  ( ArithmeticExpr (Add, Constant, Identifier, Mul),
-    BooleanExpr (LessEqual),
-    Command (Assignment, Loop),
-    Program (Program),
-    VariableDecl (IntegerDecl),
-  )
-import Shrimp.Interpreter
-  ( emptyState,
-    execute,
-  )
-
 main :: IO ()
 main = do
-  let p =
-        Program
-          [IntegerDecl "i" (Constant 1), IntegerDecl "n" (Constant 5), IntegerDecl "x" (Constant 1)]
-          [ Loop
-              (LessEqual (Identifier "i") (Identifier "n"))
-              [ Assignment "x" (Mul (Identifier "x") (Identifier "i")),
-                Assignment "i" (Add (Identifier "i") (Constant 1))
-              ]
-          ]
-  let s = execute emptyState p
-  print p
-  print s
+  print "Fried Shrimp"
