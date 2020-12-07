59dc551554da802ab317680a5498c8281300c3ce
loreloc <lorenzoloconte@outlook.it>
2020-12-07 18:26:31 +0100

    added more tests in Spec.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,34 +1,14 @@
 import Data.Maybe (fromJust, isJust)
-import Shrimp.Grammar
-  ( ArithmeticExpr (Add, Constant, Identifier, Mul),
-    BooleanExpr (LessEqual),
-    Command (Assignment, Loop),
-    Block
-  )
-import Shrimp.Interpreter
-  ( execute,
-  )
-import Shrimp.State
-  ( empty,
-    search,
-  )
-
-programFactorial :: Block
-programFactorial =
-    [ Assignment "i" (Constant 1),
-      Assignment "n" (Constant 5),
-      Assignment "x" (Constant 1),
-      Loop
-        (LessEqual (Identifier "i") (Identifier "n"))
-        [ Assignment "x" (Mul (Identifier "x") (Identifier "i")),
-          Assignment "i" (Add (Identifier "i") (Constant 1))
-        ]
-    ]
+import qualified Shrimp.Parser as Parser
+import qualified Shrimp.Interpreter as Interpreter
+import qualified Shrimp.State as State
 
 testFactorial :: IO ()
 testFactorial = do
-  let s = execute empty programFactorial
-  let v = search "x" s
+  source <- readFile "examples/factorial.shr"
+  let (program, _) = Parser.parse source
+  let state = Interpreter.run program
+  let v = State.search "x" state
   if isJust v
     then
       if fromJust v == 120
@@ -36,6 +16,36 @@ testFactorial = do
         else error "Factorial - result mismatch"
     else error "Factorial - undefined result"
 
+testFibonacci :: IO ()
+testFibonacci = do
+  source <- readFile "examples/fibonacci.shr"
+  let (program, _) = Parser.parse source
+  let state = Interpreter.run program
+  let v = State.search "x" state
+  let f = State.search "f" state
+  let g = State.search "g" state
+  if isJust v && isJust f && isJust g
+  then
+    if fromJust v == 55 && fromJust f == 34 && fromJust g == 55
+      then print "Fibonacci - passed"
+      else error "Fibonacci - result mismatch"
+  else error "Fibonacci - undefined result"
+
+testCalculator :: IO ()
+testCalculator = do
+  source <- readFile "examples/calculator.shr"
+  let (program, _) = Parser.parse source
+  let state = Interpreter.run program
+  let v = State.search "result" state
+  if isJust v
+    then
+      if fromJust v == 8
+        then print "Calculator - passed"
+        else error "Calculator - result mismatch"
+    else error "Calculator - undefined result"
+
 main :: IO ()
 main = do
   testFactorial
+  testFibonacci
+  testCalculator
