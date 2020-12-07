691d58e5b2b2741e36b404f346b484c18ac36f85
loreloc <lorenzoloconte@outlook.it>
2020-12-07 12:30:05 +0100

    added left-associative parser chain combinator

--- src/Shrimp/Parser.hs
+++ src/Shrimp/Parser.hs
@@ -7,6 +7,11 @@ newtype Parser a = Parser (String -> [(a, String)])
 parse :: Parser a -> (String -> [(a, String)])
 parse (Parser p) = p
 
+-- | Define an augment monad class
+class Monad m => AugmentMonad m where
+  zero :: m a
+  plus :: m a -> m a -> m a
+
 -- | Define the functor instance
 instance Functor Parser where
   fmap f p = Parser (\cs -> [(f a, cs') | (a, cs') <- parse p cs])
@@ -27,19 +32,14 @@ instance Monad Parser where
   return a = Parser (\cs -> [(a, cs)])
   p >>= f = Parser (\cs -> concat [parse (f a) cs' | (a, cs') <- parse p cs])
 
--- | Define an `augmented` monad class
-class Monad m => AugmentedMonad m where
-  zero :: m a
-  plus :: m a -> m a -> m a
-
--- | Define the `augmented` monad instance
-instance AugmentedMonad Parser where
+-- | Define the augment monad instance
+instance AugmentMonad Parser where
   zero = Parser (const [])
   plus p q = Parser (\cs -> parse p cs ++ parse q cs)
 
 -- | Deterministic addition of parsers
-($$) :: Parser a -> Parser a -> Parser a
-($$) p q =
+(<|>) :: Parser a -> Parser a -> Parser a
+(<|>) p q =
   Parser
     ( \cs -> case parse (p `plus` q) cs of
         [] -> []
@@ -62,6 +62,12 @@ sat p = do c <- item; if p c then return c else zero
 -- | Many combinator that repeately applicate a parser
 many :: Parser a -> Parser [a]
 many p = do a <- p; as <- many p; return (a : as)
+
+-- | Chain combinator (left-associative)
+chain :: Parser a -> Parser (a -> a -> a) -> a -> Parser a
+chain p op x = do a <- p; rest a <|> return x
+  where
+    rest a = do f <- op; b <- p; rest (f a b) <|> return a
 
 -- | Parse spaces
 space :: Parser String
